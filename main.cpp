402029d7b4cdf31e1bb24f4d8c99845435f61b35
Alejandro Zuniga <alejandro927>
2021-04-30 16:25:42 -0600

    Termine y valide lo necesario y comente :v

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+//funcion del menu
 int menu() {
 	int opcion=0;
 	cout<<"\nLas opciones son:\n";
@@ -19,32 +20,38 @@ int menu() {
 	return opcion;
 }
 
+//recursiva del primer ejercicio
 void RecursivaPascal(int max, int n, int res_anterior [], int salida []) {
+	//ccuando llegue al tamano maximo va a parar y si no va a seguir la recursiva
 	if( n == max ) {
-
 	} else {
+		
 		for(int i=0 ; i < n ; i++) {
+			//aqui valida que si es el inicio o el final lo ponga en 1
 			if( (i == 0) || (i == n-1)) {
 				salida[i] = 1;
-			} else {
+			} else { //sino es el inicio o el final suma el anterior de la respuesta anterior con la de la posicion actual de la respuesta anterior
 				salida[i] = res_anterior[i-1] + res_anterior[i];
 			}
 		}
-
+		//aqui asigno a la respuesta anteior la salida e imprimo la salida
 		for(int i=0; i < n; i++) {
 			res_anterior[i] = salida[i];
 			cout<<salida[i]<<" ";
 			salida[i]=0;
 		}
 		cout<<endl;
+		//vuelve a llamarse la recursiva
 		RecursivaPascal(max,n+1,res_anterior,salida);
 	}
 }
 
+//funcion del ejercicio 1
 void Ejercicio1() {
 	int max_n=0;
 	cout<<"Ingrese el valor max_n : ";
 	cin>>max_n;
+	//validacion de que solo acepte numeros entre 1-99
 	while((max_n<=0)||(max_n>=100)) {
 		cout<<"El valor no es permitido inngrese otro:";
 		cin>>max_n;
@@ -52,6 +59,7 @@ void Ejercicio1() {
 	int n=1;
 	int res_anterior [100];
 	int salida [100];
+	//llama la recursiva
 	RecursivaPascal(max_n+2,n,res_anterior,salida);
 }
 
@@ -62,6 +70,7 @@ int* ImprimirArr(int arr[]) {
 	return arr;
 }
 
+//llena el arreglo del ejercicio3
 int* LlenarArr(int arr[]) {
 	for(int i=0 ; i<10 ; i++) {
 		arr[i] = 1 + (rand() % 20);
@@ -69,6 +78,7 @@ int* LlenarArr(int arr[]) {
 	return arr;
 }
 
+//llena el arreglo del ejercicio2
 int* LlenarArrEjercicio2(int arr[]) {
 	for(int i=0 ; i<20 ; i++) {
 		arr[i] = 1 + (rand() % 100);
@@ -76,6 +86,7 @@ int* LlenarArrEjercicio2(int arr[]) {
 	return arr;
 }
 
+//imprime el arrglo del ejercicio2
 int* ImprimirArrEjercicio2(int arr[]) {
 	for(int i=0 ; i<20 ; i++) {
 		cout<<arr[i]<<" ";
@@ -83,7 +94,8 @@ int* ImprimirArrEjercicio2(int arr[]) {
 	return arr;
 }
 
-double promedio(int x[]){
+//la funcion paara sacar el promedio
+double Promedio(int x[]){
 	double resultado=0;
 	for(int i=0 ; i<20 ; i++) {
 		resultado += x[i];
@@ -92,6 +104,7 @@ double promedio(int x[]){
 	return resultado;
 }
 
+//Funcion de la sumatoria
 double Sumatoria(double promedio,int x[]){
 	double resultado=0;
 	double resta=0;
@@ -101,12 +114,12 @@ double Sumatoria(double promedio,int x[]){
 			resta = x[i] - promedio;
 			ecuacion = pow(resta,2);
 			resultado = resultado + ecuacion;
-			cout<<" ( "<<i<<") "<<resultado<<" ,";
+			//cout<<" ( "<<i<<") "<<resultado<<" ,";
 		}else{
 			resta = (double)promedio - x[i] ;
 			ecuacion = pow(resta,2);
 			resultado = resultado + ecuacion;
-			cout<<" ( "<<i<<") "<<resultado<<" ,";
+			//cout<<" ( "<<i<<") "<<resultado<<" ,";
 		}	
 	}
 	cout<<endl;
@@ -119,12 +132,13 @@ void Ejercicio2() {
 	double suma=0;
 	double desviacion=0;
 	LlenarArrEjercicio2(arreglo);
+	cout<<"El arreglo es:"<<endl;
 	ImprimirArrEjercicio2(arreglo);
 	cout<<endl;
-	prom = promedio(arreglo);
-	cout<<"Promedio:"<<prom<<endl;
+	prom = Promedio(arreglo);
+	//cout<<"Promedio:"<<prom<<endl;
 	suma = Sumatoria(prom,arreglo);
-	cout<<"La suma es:"<<suma<<endl;
+	//cout<<"La suma es:"<<suma<<endl;
 	desviacion = sqrt(suma/20);
 	cout<<"La desviacion es de:"<< desviacion<<endl;
 	
@@ -138,19 +152,11 @@ void Primero(int x[], int y[]){
 		for(int j=0; j < x[i]; j++){
 			cout<<val<<" ";
 		}
-		cout<<endl;
+		cout<<endl<<endl;
 		for(int j=0; j < y[i]; j++){
 			cout<<val2<<" ";
 		}
-		cout<<endl;
-	}
-}
-void Segundo(int x[]){
-	char val = 178;
-	for(int i=0 ; i < 10 ; i++){
-		for(int j=0; j < x[i]; j++){
-			cout<<val<<endl;
-		}
+		cout<<endl<<endl;
 	}
 }
 
@@ -164,7 +170,6 @@ void Ejercicio3() {
 	ImprimirArr(arreglo2);
 	cout<<endl;
 	Primero(arreglo1,arreglo2);
-
 }
 
 
